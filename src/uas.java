2dd344de56c5478fc96280b3ea2482f0e71d18b7
Lenovo <aprilianapramesti37@gmail.com>
2023-01-17 11:59:39 +0700

    OK

--- src/uas.java
+++ src/uas.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class uas {
-}
+public class uas {
+    public void ShowPosts(UAS1E profile){;
+        //
+
+        String social=new UAS1E;
+        String ;
+        System.out.println("pulpen")
+        System.out.println("buku tulis"); ;
+        System.out.println("buku gambar"); ;
+        System.out.println("pensil"); ;
+        System.out.println("penghapus"); ;
+
